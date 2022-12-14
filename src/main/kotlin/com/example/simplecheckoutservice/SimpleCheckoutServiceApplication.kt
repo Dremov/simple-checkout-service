aa8e45847569c27a82077059af6868027da8ac1c
Dremov, Andrew (PPN) <andrii.dremov@thoughtworks.com>
2022-12-14 19:11:43 +0100

    initial commit

---
+++ src/main/kotlin/com/example/simplecheckoutservice/SimpleCheckoutServiceApplication.kt
@@ -0,0 +1,11 @@
+package com.example.simplecheckoutservice
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class SimpleCheckoutServiceApplication
+
+fun main(args: Array<String>) {
+    runApplication<SimpleCheckoutServiceApplication>(*args)
+}
---
+++ src/test/kotlin/com/example/simplecheckoutservice/SimpleCheckoutServiceApplicationTests.kt
@@ -0,0 +1,13 @@
+package com.example.simplecheckoutservice
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class SimpleCheckoutServiceApplicationTests {
+
+    @Test
+    fun contextLoads() {
+    }
+
+}
